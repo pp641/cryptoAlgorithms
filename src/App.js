4367988804c5fffb95baee6dc7a4c3434a9d1d83
Prajjwal Pandey <prajjwalpandey641@gmail.com>
2022-01-07 10:43:27 +0530

    topic added

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import AllHashingAlgorithms from "./HashingAlgrithms/AllHashingAlgorithms";
 function App() {
   return (
     <div>
+      <h1>Encryption Algorithms :: </h1>
       <AllCryptoAlgorithms />
+      <h1>Hashing Algorithms :: </h1>
       <AllHashingAlgorithms />
     </div>
   );
